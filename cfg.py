0fdad8d3da28957f8207881e2285e3094949940d
Brian Zhang <zhangbr1@northvilleschools.net>
2023-06-26 19:35:49 -0400

    added flowers102 to data

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import os
 
-base_path = f"{os.getenv('HOME')}/workspace"
+base_path = f"{os.getenv('HOME')}/ilm-vp/ILM-VP-brian"
 
 data_path = os.path.join(base_path, "data")
 results_path = os.path.join(base_path, "projects", "ILM-VP", "results")
